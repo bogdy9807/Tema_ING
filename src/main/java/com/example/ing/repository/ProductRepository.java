27f68550e3cc75c7b061882046f638d823c60b45
Bogdan Csep <bogdi_1998@yahoo.com>
2023-12-08 14:05:08 +0200

    added implementation for add and find product

--- src/main/java/com/example/ing/repository/ProductRepository.java
+++ src/main/java/com/example/ing/repository/ProductRepository.java
@@ -4,6 +4,10 @@ import com.example.ing.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+	Optional<ProductEntity> findByExternalId(String externalId);
 }
--- src/main/java/com/example/ing/service/ProductService.java
+++ src/main/java/com/example/ing/service/ProductService.java
@@ -1,9 +1,16 @@
 package com.example.ing.service;
 
+import com.example.ing.api.dto.ProductDto;
+import com.example.ing.entity.ProductEntity;
+import com.example.ing.mapper.ProductMapper;
 import com.example.ing.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -11,4 +18,24 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
 	private final ProductRepository productRepository;
+
+	public ProductDto getProduct(String externalId) {
+		log.info("getProduct({})", externalId);
+		Optional<ProductEntity> productEntity = productRepository.findByExternalId(externalId);
+		ProductDto productDto = ProductMapper.mapToProductDto(productEntity.orElse(new ProductEntity()));
+		log.debug("getProduct() finished: {}", productDto);
+		return productDto;
+	}
+
+	public String addProduct(ProductDto productDto) {
+		log.info("addProduct({})", productDto);
+		ProductEntity productEntity = ProductMapper.mapToProductEntity(productDto);
+		if (Strings.isBlank(productEntity.getExternalId())) {
+			productEntity.setExternalId(UUID.randomUUID().toString());
+		}
+		productRepository.save(productEntity);
+		String externalId = productEntity.getExternalId();
+		log.debug("addProduct() finished: {}", externalId);
+		return externalId;
+	}
 }
